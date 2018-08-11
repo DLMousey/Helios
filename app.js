b2e631c78ec9262d5e210fa7a709eb17d924cd9f
DLMousey <tom@llamatorials.com>
2018-08-11 17:41:00 +0100

    Add angular app + express routing

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const db = require('./database/connection');
 const dbConfig = require('./config/db.config');
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
+const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname + '/client-app/dist')));
+
 app.use('/api', apiRouter);
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/spa.html');
+    res.sendFile(path.join(__dirname, '/client-app/dist/index.html'));
 });
 
 db.connect(dbConfig.getUrl(), (err) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log('index route fired');
+  res.sendFile(path.resolve(__dirname + '/client-app/dist/index.html'));
 });
 
 module.exports = router;
